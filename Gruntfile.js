ba06641700659014f277708727971cc1d18fde16
qingyun <codingtsunami@gmail.com>
2014-05-16 17:38:58 +0800

    Configure gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,39 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-	})
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        stylus: {
+            compile: {
+                files: {
+                    'public/stylesheets/style.css': ['public/stylesheets/*.styl']
+                }
+            }
+        },
+        jshint: {
+            options: {
+                asi: true,
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                undef: true,
+                sub: true,
+                white: true,
+                node: true,
+                browser: true,
+            },
+            files: ['routes/*.js', './*.js', 'public/javascripts/*.js', 'model/*.js'],
+        },
+        watch: {
+            scripts: {
+                files: ['public/stylesheets/style.styl', 'routes/*.js', './*.js', 'public/javascripts/*.js', 'model/*.js'],
+                tasks: ['stylus', 'jshint'] 
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-stylus')
+    grunt.loadNpmTasks('grunt-contrib-jshint')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+
+    // grunt.registerTask('default', ['stylus'])
 }
