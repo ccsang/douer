adb6ea78818df515fd9d83a7adc40a3a786deb8a
qingyun <codingtsunami@gmail.com>
2014-05-16 15:15:02 +0800

    Build the skeleton

---
+++ create.sql
@@ -0,0 +1,118 @@
+create table if not exists user_info (
+    id int primary key not null auto_increment,
+    email varchar(128) not null,
+    password varchar(128) not null,
+    nickname varchar(128) not null,
+    sex int not null default 0,
+    birthday date,
+    city varchar(128),
+    status int not null default 1,
+    hometown varchar(128),
+    school varchar(128),
+    private_set int not null default 1,
+    photo varchar(128),
+    register_time timestamp not null default CURRENT_TIMESTAMP
+);
+
+create table if not exists friends (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    group_id int not null,
+    comment varchar(128) 
+);
+
+create table if not exists friends_group (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    group_name varchar(128) not null
+);
+
+create table if not exists add_friend_msg (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    send_user_id int not null,
+    send_time timestamp not null default CURRENT_TIMESTAMP,
+    content varchar(255)
+);
+
+create table if not exists short_messages (
+    id int primary key not null auto_increment,
+    send_user_id int not null,
+    receive_user_id int not null,
+    send_time timestamp not null default CURRENT_TIMESTAMP,
+    content varchar(255) not null,
+    status int 
+);
+
+create table if not exists visit_record (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    friend_id int not null,
+    time timestamp not null default CURRENT_TIMESTAMP
+);
+
+create table if not exists blog (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    category_id int not null,
+    title varchar(128) not null,
+    post_time timestamp not null default CURRENT_TIMESTAMP,
+    update_time timestamp not null 
+);
+
+create table if not exists category (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    category_name varchar(128) not null   
+);
+
+create table if not exists photo (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    album_id int not null,
+    path varchar(128) not null,
+    comment varchar(128)
+);
+
+create table if not exists album (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    album_name varchar(128) not null
+);
+
+create table if not exists message (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    send_user_id int not null,
+    content varchar(128) not null,
+    post_time timestamp not null default CURRENT_TIMESTAMP
+);
+
+create table if not exists broadcast (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    content varchar(255) not null,
+    post_time timestamp not null default CURRENT_TIMESTAMP 
+);
+
+create table if not exists share (
+    id int primary key not null auto_increment,
+    user_id int not null,
+    content varchar(255)
+);
+
+create table if not exists review (
+    id int primary key not null auto_increment,
+    type int not null,
+    review_id int not null,
+    user_id int not null,
+    content varchar(255),
+    review_time timestamp not null default CURRENT_TIMESTAMP
+);
+
+create table if not exists review_type (
+    id int primary key not null auto_increment,
+    name varchar(128) not null 
+);
+
+
---
+++ drop.sql
@@ -0,0 +1,16 @@
+drop table if exists add_friend_msg;
+drop table if exists album;
+drop table if exists blog;
+drop table if exists broadcast;
+drop table if exists category;
+drop table if exists friends;
+drop table if exists friends_group;
+drop table if exists message;
+drop table if exists photo;
+drop table if exists review;
+drop table if exists review_type;
+drop table if exists share;
+drop table if exists short_messages;
+drop table if exists user_info;
+drop table if exists visit_record;
+ 
